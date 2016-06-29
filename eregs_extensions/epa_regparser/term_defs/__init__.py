124c8089cd915394cc9f19074af0e2f3d9daf8b9
Tadhg O'Higgins <tadhg.ohiggins@gsa.gov>
2016-06-28 17:02:41 -0700

    Add term definitions for “CROMERR Costs”. Fixes <https://github.com/18F/eregs-platform/issues/28>.

---
+++ eregs_extensions/epa_regparser/term_defs/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+term_defs = {
+    "264": [
+        ("CROMERR Costs",
+         "CROMERR Costs")
+    ],
+    "265": [
+        ("CROMERR Costs",
+         "CROMERR Costs")
+    ]
+}
+
+ignores = {
+}
